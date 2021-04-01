1d98f072132ed26eb9e0c42cac242c4994f1b5e0
leorrose <Leor.rose@gmail.com>
2021-04-01 17:45:29 +0300

    add coverage

--- app/client/src/app/services/rest-api.service.spec.ts
+++ app/client/src/app/services/rest-api.service.spec.ts
@@ -1,18 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { HttpClientTestingModule} from '@angular/common/http/testing';
 import { RestApiService } from './rest-api.service';
 
 
 describe('RestApiService', () => {
     let service: RestApiService;
-    let httpTestingController: HttpTestingController;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ HttpClientTestingModule ]
         });
         service = TestBed.inject(RestApiService);
-        httpTestingController = TestBed.inject(HttpTestingController);
     });
 
     it('should be created', () => {
